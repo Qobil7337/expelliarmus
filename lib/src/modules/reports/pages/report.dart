afb499d03543873d669107edde295050a80cbde2
Qobil7337 <qobil7337@mail.ru>
2025-10-13 02:23:19 +0500

    add toggleSwitch

--- lib/src/modules/reports/pages/report.dart
+++ lib/src/modules/reports/pages/report.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class Report extends StatefulWidget {
   const Report({super.key});
@@ -8,8 +9,42 @@ class Report extends StatefulWidget {
 }
 
 class _ReportState extends State<Report> {
+  int selectedIndex = 1;
+  final items = ['Haftalik', 'Oylik', 'Yillik'];
+
   @override
   Widget build(BuildContext context) {
-    return Text('Report page');
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(241, 242, 245, 1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ToggleSwitch(
+          cornerRadius: 10.0,
+          radiusStyle: true,
+          initialLabelIndex: selectedIndex,
+          totalSwitches: 3,
+          labels: items,
+          minWidth: 120,
+          minHeight: 30,
+          borderColor: [Colors.transparent],
+          inactiveBgColor: Color.fromRGBO(241, 242, 245, 1),
+          inactiveFgColor: Colors.grey[700],
+          activeBgColors: const [
+            [Colors.white],
+            [Colors.white],
+            [Colors.white],
+          ],
+          activeFgColor: Colors.black,
+          onToggle: (index) {
+            setState(() {
+              selectedIndex = index!;
+            });
+          },
+        ),
+      ),
+    );
   }
 }
